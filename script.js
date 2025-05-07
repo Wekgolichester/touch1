9d52ea3bb5b07db95cf9b432bb0183c1385595ed
Wekgolichester <99594150+Wekgolichester@users.noreply.github.com>
2025-05-07 14:37:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ function showMemory(key) {
 
 function createTextImage(text, padding = 8, fontSize = 7) {
   const ratio = window.devicePixelRatio || 1;
-  const font = `${fontSize}px 'Noto Sans KR', sans-serif`;
+  const font = `${fontSize}px 'ChosunGu'`;
 
   const measureCanvas = document.createElement("canvas");
   const measureCtx = measureCanvas.getContext("2d");
